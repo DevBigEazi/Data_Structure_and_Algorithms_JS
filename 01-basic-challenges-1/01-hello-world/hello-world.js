c17735bf00a88edbe8aa465bcbc9a5afd964bdf5
DevBigEazi <adesholatajudeen1@gmail.com>
2023-10-29 12:42:14 +0100

    Challenge 1, 2, 3 done

--- 01-basic-challenges-1/01-hello-world/hello-world.js
+++ 01-basic-challenges-1/01-hello-world/hello-world.js
@@ -3,3 +3,4 @@ function helloWorld() {
 }
 
 module.exports = helloWorld;
+//
--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,24 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    let results;
+
+    switch (operator) {
+        case "+":
+            results = num1 + num2;
+            break;
+        case "-":
+            results = num1 - num2;
+            break;
+        case "*":
+            results = num1 * num2;
+            break;
+        case "/":
+            results = num1 / num2;
+            break;
+        default:
+
+    }
+
+    return results;
+}
 
 module.exports = calculator;
