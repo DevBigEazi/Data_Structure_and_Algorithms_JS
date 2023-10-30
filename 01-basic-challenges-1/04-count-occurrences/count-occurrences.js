8f19911d863c0b19b48ee67c371e69d3864e26ab
DevBigEazi <adesholatajudeen1@gmail.com>
2023-10-30 10:39:36 +0100

    Basic Challenge 4 done

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    let count = 0
+
+    for (const st of str) {
+        if (st === char)
+            count++;
+    }
+    // console.log(count)
+    return count;
+}
 
 module.exports = countOccurrences;
